a87d96db1f7bfdc155ac91b08dd71f0e6e54b2ee
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-02-09 11:30:08 +0700

    day 1 and day 2

---
+++ day1.js
@@ -0,0 +1,96 @@
+// 1. Find area of rectangle
+// Make Variable
+// const length = 5;
+// const width = 3;
+
+// Final
+// const areaRectangle = length * width
+// console.log(length * width)
+
+// 2. Find perimeter of rectangle
+//Make Variable
+// const length = 5;
+// const width = 3;
+
+// Total = (sisi2 + sisi2)
+// const length2 = length * 2;
+// console.log(length2);
+
+// const width2 = width * 2;
+// console.log(width2);
+
+// const totalPerimeter = length2 + width2;
+// console.log(totalPerimeter);
+
+// 3. Find diameter, circumference, and area of circle
+// const r = 5;
+// const phi = 3.14;
+
+// d = 2r, c = 2πr, A = πr² 
+// const d = 2 * r;
+// console.log(d);
+
+// const c = 2 * phi * r;
+// console.log(c);
+
+// const a = phi * r ** 2
+// console.log(a);
+
+// 4. Find angles of triangles
+// const a = 80;
+// const b = 65;
+
+// angle = 180 - a - b
+// const c = 180 - a - b;
+// console.log(c);
+
+// 5. Date 
+// const date1 = new Date ('2022-01-20');
+// const date2 = new Date ('2022-01-22');
+
+// const selisih = Math.abs(date1 - date2)
+
+//selisih dalam milisecond
+// const hariDalamMiliSecond = 24 * 3600 * 1000
+// const selisihTanggal = Math.round(selisih/hariDalamMiliSecond)
+
+// console.log(selisihTanggal);
+
+// 6. Convert days 
+// 1 year = 365 hari, 1 month = 30 hari;
+// const days = 700; 
+// const year = 365;
+// const month = 30;
+
+//days = total days - year - total month
+
+// const resultYear = Math.round(days / year);
+// console.log(resultYear);
+
+// const resultMonth = Math.floor(days % year / month); 
+// console.log(resultMonth);
+
+// const resultDays = Math.round(days - year) % month
+// console.log(resultDays)
+
+// console.log(`${resultYear} year, ${resultMonth} month, ${resultDays} days`);
+
+// const days = 366; 
+// const year = 365;
+// const month = 30;
+
+// const resultYear = Math.round(days / year);
+// console.log(resultYear);
+
+// const resultMonth = Math.floor(days % year / month); 
+// console.log(resultMonth);
+
+// const resultDays = Math.round(days - year) % month
+// console.log(resultDays)
+
+// console.log(`${resultYear} year, ${resultMonth} month, ${resultDays} days`);
+
+
+
+
+
---
+++ day2.js
@@ -0,0 +1,89 @@
+// 1. Convert Celcius to Fahrenheit
+// const celcius = 60;
+// if (celcius) {
+//     const Fahrenheit = celcius * 1.8 + 32;
+//     console.log(Fahrenheit);
+// };
+
+// const celcius = 60;
+// const result = (celcius * 9) / 5 + 32;
+
+// console.log('cel to fah :', result)
+
+// 2. Odd Numer or Even Number
+// const angka = 25; // atau 2
+// if (angka % 2 === 0) {
+//     console.log ('Odd Number');
+// } else {
+//     console.log ('Even Number');
+// };
+
+// const angka = 25;
+
+// console.log(angka % 2 === 0 ? 'genap' : 'ganjil')
+
+// 3. Prime Number or Not
+// const angka = 9;
+
+// if (angka <= 1) {
+//   return console.log("not prime number");
+// }
+// if (angka === 2) {
+//   return console.log("prime number");
+// }
+// for (let i = 2; i < angka; i++) {
+//   if (angka % i === 0) {
+//     return console.log("not prime number");
+//   }
+//   if (angka % 3 === 0 && angka != 3) {
+//     return console.log("not prime number");
+//   }
+//   return console.log("prime number");
+// }
+
+// const num = 9;
+// let isPrime = true;
+
+// for (let i = 2; i < num; i++) {
+//   if (num % i == 0) {
+//     isPrime = false;
+//     break;
+//   }
+// }
+
+// console.log(isPrime);
+
+// 4. Sum Of the Numbers
+// const angka = 5; 
+// let sum = 0;
+
+// for (let i = 1; i <= angka; i++) {
+//     sum += i
+// }
+// console.log(sum);
+
+
+// 5. Factorial Number
+// const angka = 4;
+// let sum = 1;
+
+// for (let i = 1; i <= angka; i++) {
+//     sum *= i
+// };
+// console.log(sum);
+
+// 6. Fibonnaci Numbers
+
+// const fn = 15
+// let a = 0;
+// let b = 1;
+// let c ;
+
+
+// for (let i = 2; i <= fn; i++) {
+//        c = a + b;
+//        a = b;
+//        b = c
+// };
+
+// console.log (c);
