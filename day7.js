c04a73f394211289838d5fc77a4ee560ed77f99a
AbhimantraPNW <158009006+AbhimantraPNW@users.noreply.github.com>
2024-02-19 18:05:10 +0700

    day7.js

--- day7.js
+++ day7.js
@@ -7,14 +7,34 @@
 //   }
 // }
 
-// const obj1 = equal({ a: 1 }, { a: 2 });
-// console.log(obj1);
+// const result = equal({ a: 1 }, { a: 2 });
+// console.log(result);
+
+//Or
+// function areObjectEquals(obj1, obj2) {
+//   const key1 = Object.keys(obj1);
+//   const key2 = Object.keys(obj2);
+
+//   if (key1.length !== key2.length) {
+//     return false;
+//   }
+//   for (const key of key1) {
+//     if (obj1[key] !== obj2[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// const arg1 = { a: 2 };
+// const arg2 = { a: 1 };
+// console.log(areObjectEquals(arg1, arg2));
 
 // 2. Create a function to get the intersection of two objects
 // const obj1 = { a: 1, b: 2 };
 // const obj2 = { a: 1, c: 3 };
 
-// function inters(o1, o2) {
+// function inters(object) {
 //   const intersection = {};
 //   for (const key in obj1) {
 //     if (obj2.hasOwnProperty(key)) {
@@ -24,20 +44,34 @@
 //   return intersection;
 // }
 
-// const commonProperties = inters(obj1, obj2);
+// const commonProperties = inters();
 // console.log(commonProperties);
 
+//Or
+// const intersection = (obj1, obj2) => {
+//   const duplicate = {};
+
+//   for (let key in obj1) {
+//     if (obj1[key] === obj2[key]) {
+//       duplicate[key] = obj1[key];
+//     }
+//   }
+//   return duplicate;
+// };
+
+// const arg1 = { a: 1, b: 2 };
+// const arg2 = { a: 1, c: 3 };
+// console.log(intersection(arg1, arg2));
+
 // 3. Create a function to merge two array of student data and remove duplicate data
-// const array1 = [
-//   { name: "Student 1", email: "student1@mail.com" },
-//   { name: "Student 2", email: "student2@mail.com" },
-// ];
-
-// const array2 = [
-//   { name: "Student 1", email: "student1@mail.com" },
-//   { name: "Student 3", email: "student3@mail.com" },
-// ];
-
+// const arr1 =  [
+//     { name: 'Student 1', email : 'student1@mail.com' },
+//     { name: 'Student 2', email : 'student2@mail.com' }
+//     ]
+// const arr2 =  [
+//     { name: 'Student 1', email : 'student1@mail.com' },
+//     { name: 'Student 3', email : 'student3@mail.com' }
+//     ]
 // function mergeArr(arr1, arr2) {
 //   let mergeArr = array1.concat(array2);
 
@@ -48,6 +82,34 @@
 // }
 
 // console.log(mergeArr(array1, array2));
+
+//Or
+// const removeDuplicate = (arr1, arr2) => {
+//   const combineArray = [...arr1, ...arr2];
+
+//   const temp = [];
+
+//   for (let i = 0; i < combineArray.length; i++) {
+//     const duplicateValues = temp.filter((val) => {
+//       return val.email === combineArray[i].email;
+//     });
+//     if (!duplicateValues.length) {
+//       temp.push(combineArray[i]);
+//     }
+//   }
+//   return temp;
+// };
+
+// const arr1 = [
+//   { name: "Student 1", email: "student1@mail.com" },
+//   { name: "Student 2", email: "student2@mail.com" },
+// ];
+// const arr2 = [
+//   { name: "Student 1", email: "student1@mail.com" },
+//   { name: "Student 3", email: "student3@mail.com" },
+// ];
+
+// console.log(removeDuplicate(arr1, arr2));
 
 // 4. Create a function that can accept input as an array of objects and switch all values into property and
 // property into value
@@ -66,6 +128,23 @@
 
 // console.log(switchProperty([{ name: "David", age: 20 }]));
 
+// Or
+// const switchValueToKey = (arr) => {
+//   const result = [];
+
+//   arr.forEach((item) => {
+//     let temp = {};
+
+//     for (let key in item) {
+//       temp[item[key]] = key;
+//     }
+//     result.push(temp);
+//   });
+//   return result;
+// };
+
+// console.log(switchValueToKey([{ name: "David", age: 20}]));
+
 // 5. Create a function to find a factorial number using recursion
 // function factorial(num) {
 //   if (num === 0) {
@@ -75,5 +154,26 @@
 //   }
 // }
 
-// const n = 5;
-// console.log(factorial(n));
+// console.log(factorial(5));
+
+// Or
+// const factorial = (n) => {
+//   if (n === 1) {
+//     return {
+//       steps: ["1"],
+//       total: 1,
+//     };
+//   } else {
+//     const next = factorial(n - 1);
+
+//     return {
+//       steps: [n, ...next.steps],
+//       total: n * next.total,
+//     };
+//   }
+// };
+
+// const number = 5;
+// const { steps, total } = factorial(number);
+
+// console.log(steps.join(" x ") + " = " + total);
